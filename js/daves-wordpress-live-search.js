97a3a31e3905f00dea82e2015df8fec3556c6550
Dave Ross <dave@davidmichaelross.com>
2011-09-01 20:57:00 -0500

    The LiveSearch.util class isn't being used anymore. Buh-bye.

--- js/daves-wordpress-live-search.js
+++ js/daves-wordpress-live-search.js
@@ -293,38 +293,6 @@ LiveSearch.removeIndicator = function() {
 	jQuery("#search_results_activity_indicator").remove();
 };
 
-/////////////
-// Utilities
-/////////////
-LiveSearch.util = {};
-
-/**
- * Returns the filename component of the path.
- * Used here to help find this script's <script> tag
- * @see http://phpjs.org/functions/basename
- * @see http://www.experts-exchange.com/Programming/Languages/Scripting/JavaScript/Q_24662495.html
- * @return string
- */
-LiveSearch.util.basename = function(path, suffix) {
-    // Returns the filename component of the path  
-    // 
-    // version: 909.322
-    // discuss at: http://phpjs.org/functions/basename
-    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: Ash Searle (http://hexmen.com/blog/)
-    // +   improved by: Lincoln Ramsay
-    // +   improved by: djmix
-    // *     example 1: basename('/www/site/home.htm', '.htm');
-    // *     returns 1: 'home'
-    var b = path.replace(/^.*[\/\\]/g, '');
-    
-    if (typeof(suffix) == 'string' && b.substr(b.length-suffix.length) == suffix) {
-        b = b.substr(0, b.length-suffix.length);
-    }
-    
-    return b;
-}
-
 ///////////////////
 // Initialization
 ///////////////////
